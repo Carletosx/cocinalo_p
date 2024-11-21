6a00fe6949d757a47a38144b0758ae0b72390214
Carletosx <carlosferreiraernesto@gmail.com>
2024-11-21 00:17:04 -0500

    configuracion de agendacion y formato de calendario 100% terminado

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { MealProvider } from './context/mealContext';
 import { SidebarProvider } from './context/sidebarContext';
@@ -10,54 +10,55 @@ import { Home, Error, MealDetails, Category, CalendarPage } from './pages';
 import LoginForm from './components/Auth/LoginForm';
 import RegisterForm from './components/Auth/RegisterForm';
 
+const AppContent = () => {
+    const location = useLocation();
+    const isCalendarPage = location.pathname === '/calendar';
+
+    return (
+        <div className='main-holder'>
+            {isCalendarPage ? (
+                <>
+                    <Header showOnlyNavbar={true} />
+                    <main className='main-content'>
+                        <Routes>
+                            <Route 
+                                path="/calendar" 
+                                element={
+                                    <ProtectedRoute>
+                                        <CalendarPage />
+                                    </ProtectedRoute>
+                                } 
+                            />
+                        </Routes>
+                    </main>
+                </>
+            ) : (
+                <>
+                    <Header />
+                    <Sidebar />
+                    <main className='main-content'>
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/meal/:id" element={<MealDetails />} />
+                            <Route path="/meal/category/:name" element={<Category />} />
+                            <Route path="/login" element={<PublicRoute><LoginForm /></PublicRoute>} />
+                            <Route path="/register" element={<PublicRoute><RegisterForm /></PublicRoute>} />
+                            <Route path="*" element={<Error />} />
+                        </Routes>
+                    </main>
+                </>
+            )}
+        </div>
+    );
+};
+
 function App() {
     return (
         <AuthProvider>
             <MealProvider>
                 <SidebarProvider>
                     <BrowserRouter>
-                        <div className='main-holder'>
-                            <Header />
-                            <Sidebar />
-                            <main className='main-content'>
-                                <Routes>
-                                    {/* Rutas públicas */}
-                                    <Route path="/" element={<Home />} />
-                                    <Route path="/meal/:id" element={<MealDetails />} />
-                                    <Route path="/meal/category/:name" element={<Category />} />
-
-                                    {/* Rutas de autenticación */}
-                                    <Route 
-                                        path="/login" 
-                                        element={
-                                            <PublicRoute>
-                                                <LoginForm />
-                                            </PublicRoute>
-                                        } 
-                                    />
-                                    <Route 
-                                        path="/register" 
-                                        element={
-                                            <PublicRoute>
-                                                <RegisterForm />
-                                            </PublicRoute>
-                                        } 
-                                    />
-
-                                    {/* Rutas protegidas */}
-                                    <Route 
-                                        path="/calendar" 
-                                        element={
-                                            <ProtectedRoute>
-                                                <CalendarPage />
-                                            </ProtectedRoute>
-                                        } 
-                                    />
-
-                                    <Route path="*" element={<Error />} />
-                                </Routes>
-                            </main>
-                        </div>
+                        <AppContent />
                     </BrowserRouter>
                 </SidebarProvider>
             </MealProvider>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,15 +3,17 @@ import "./Header.scss";
 import Navbar from "./Navbar";
 import SearchForm from "./SearchForm";
 
-const Header = () => {
+const Header = ({ showOnlyNavbar = false }) => {
   return (
-    <header className='header'>
+    <header className={`header ${showOnlyNavbar ? 'navbar-only' : ''}`}>
       <Navbar />
-      <div className='header-content flex align-center justify-center flex-column text-center'>
-        <SearchForm />
-        <h1 className='text-white header-title ls-2'>¿Cuáles son tus Recetas favoritas?</h1>
-        <p className='text-uppercase text-white my-3 ls-1'>Personaliza tu experiencia</p>
-      </div>
+      {!showOnlyNavbar && (
+        <div className='header-content flex align-center justify-center flex-column text-center'>
+          <SearchForm />
+          <h1 className='text-white header-title ls-2'>¿Cuáles son tus Recetas favoritas?</h1>
+          <p className='text-uppercase text-white my-3 ls-1'>Personaliza tu experiencia</p>
+        </div>
+      )}
     </header>
   )
 }
